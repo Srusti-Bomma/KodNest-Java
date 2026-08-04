e18e31660b2c33454ac210c5b7fcfdb914c6cfe5
Srusti-Bomma <srustibomma@gmail.com>
2026-08-04 11:41:00 +0530

    days modification

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,7 +1,7 @@
-public class WelcomeToJava{
-    public static void main(String[] args){
-        System.out.println("Welcome to Java Track!");
-        System.out.println("I already know how to build programming logic.");
-        System.out.println("Now i am learning hot to express that logic using Java");
+
+public class WelcomeToJava {
+
+    public static void main(String[] args) {
+
     }
 }
---
+++ track/M01-understanding-java/practice.java
@@ -0,0 +1,14 @@
+
+public class practice {
+
+    public static void main(String[] args) {
+        System.out.println("Monday");
+        System.out.print("Tuesday");
+        System.out.println("Wednesday");
+        System.out.println("Thursday");
+        System.out.println("Friday");
+        System.out.print("Saturday");
+        System.out.println("Sunday");
+
+    }
+}
