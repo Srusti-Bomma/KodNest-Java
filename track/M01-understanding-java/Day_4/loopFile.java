f5bb67f1676f74b07e3d7384c10ac4290ff0ea9f
Srusti-Bomma <srustibomma@gmail.com>
2026-08-06 11:40:42 +0530

    added comments abt loop

--- track/M01-understanding-java/Day_4/loopFile.java
+++ track/M01-understanding-java/Day_4/loopFile.java
@@ -3,11 +3,16 @@ public class loopFile {
 
     public static void main(String[] args) {
         //For loop
+        //use it when you know how many times the loop will run
+        //condition -->where to stop
+        //initialization -->where to start
+        //increment/decrement -->how to move to next iteration
         for (int i = 1; i <= 5; i++) {
             System.out.println(i);
         }
 
         //while loop
+        //use it when you don't know how many times the loop will run because the condition is based on some variable that changes 
         int j = 1;
         while (j <= 5) {
             System.out.println(j);
@@ -15,6 +20,8 @@ public class loopFile {
         }
 
         //do-while loop
+        //use it when you don't know how many times the loop will run because the condition is based on some variable that changes
+        //even if the condition is false, the loop will run once
         int k = 1;
         do {
             System.out.println(k);
@@ -22,6 +29,9 @@ public class loopFile {
         } while (k <= 5);
 
         //Nested for loop
+        //use it when you want to print something in rows and columns
+        //the outer loop controls the number of rows
+        //the inner loop controls the number of columns
         for (int m = 1; m <= 5; m++) {
             for (int n = 1; n <= m; n++) {
                 System.out.print(n + "");
@@ -30,6 +40,10 @@ public class loopFile {
         }
 
         //Enhanced for loop
+        //use it when you want to iterate through an array or collection
+        //Syntax : for(dataType variable : arrayName) {
+        //    System.out.println(variable);
+        //}
         int[] arr = {1, 2, 3, 4, 5};
         for (int i : arr) {
             System.out.println(i);
